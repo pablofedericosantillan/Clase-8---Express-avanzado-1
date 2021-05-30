bd9dc56d3ff8a6ae04990c6f721f067c921dced8
CHICHO <73852276+pablofedericosantillan@users.noreply.github.com>
2021-05-30 12:56:02 -0300

    Update productos.js

--- api/productos.js
+++ api/productos.js
@@ -2,16 +2,31 @@ class Productos {
     constructor(){
         this.item = [];
         }
-
-    guardar(newProduct){
+    listarTodos(){
             try{
-                newProduct.id=this.item.length+1;
-                this.item.push(newProduct);
-                return this.item;
-
+                return this.item.length!=0 ? this.item : {error : 'no hay productos cargados'}
+                
                }catch(err){
-                console.log('Hubo un error en la funcion guardar'); 
+                console.log('Hubo un error en la funcion leer todo'); 
                }
+    }
+    BuscarId(id){
+        try{
+            return id <= this.item.length ? this.item[id-1] : {error: "producto no encontrado'" }
+            
+           }catch(err){
+            console.log('Hubo un error en la funcion Buscar por ID'); 
+           }
+    }
+    guardar(newProduct){
+        try{
+            newProduct.id=this.item.length+1;
+            this.item.push(newProduct);
+            return this.item;
+
+           }catch(err){
+            console.log('Hubo un error en la funcion guardar'); 
+           }
     }
  }
 
